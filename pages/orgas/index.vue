7e8292dda25fc7d8ee72b5270ea6dcbeb0f10011
Kenan Henzelin <135265245+Eiafuawn@users.noreply.github.com>
2026-03-03 15:26:32 +0000

    chore: replaced emojis in orga page

--- pages/orgas/index.vue
+++ pages/orgas/index.vue
@@ -49,15 +49,15 @@ async function handleRegister() {
       <h3 class="section-label">Pourquoi rejoindre ?</h3>
       <div class="why-grid">
         <div class="why-card">
-          <span class="why-icon">📅</span>
+          <span class="why-icon"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Pixelarticons by Gerrit Halfmann - https://github.com/halfmage/pixelarticons/blob/master/LICENSE --><path fill="currentColor" d="M15 2h2v2h4v18H3V4h4V2h2v2h6zM9 6H5v2h14V6zm-4 4v10h14V10zm2 2h2v2H7zm6 0h-2v2h2zm2 0h2v2h-2zm-6 4H7v2h2zm2 0h2v2h-2zm6 0h-2v2h2z"/></svg></span>
           <p class="why-text">Publie tes évènements directement dans le calendrier romand.</p>
         </div>
         <div class="why-card">
-          <span class="why-icon">🔗</span>
+          <span class="why-icon"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Pixelarticons by Gerrit Halfmann - https://github.com/halfmage/pixelarticons/blob/master/LICENSE --><path fill="currentColor" d="M10 2h4v4h-4zM7 7h10v2h-2v13h-2v-6h-2v6H9V9H7zM5 5v2h2V5zm0 0H3V3h2zm14 0v2h-2V5zm0 0V3h2v2z"/></svg></span>
           <p class="why-text">Sois visible auprès des militantexs de toute la région.</p>
         </div>
         <div class="why-card">
-          <span class="why-icon">✊</span>
+          <span class="why-icon"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Pixelarticons by Gerrit Halfmann - https://github.com/halfmage/pixelarticons/blob/master/LICENSE --><path fill="currentColor" d="M9 2H5v2H3v2H1v6h2v2h2v2h2v2h2v2h2v2h2v-2h2v-2h2v-2h2v-2h2v-2h2V6h-2V4h-2V2h-4v2h-2v2h-2V4H9zm0 2v2h2v2h2V6h2V4h4v2h2v6h-2v2h-2v2h-2v2h-2v2h-2v-2H9v-2H7v-2H5v-2H3V6h2V4z"/></svg></span>
           <p class="why-text">Converge avec d'autres luttes. Soyons solidaires.</p>
         </div>
       </div>
