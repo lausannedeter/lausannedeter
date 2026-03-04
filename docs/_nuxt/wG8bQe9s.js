1cdb971e7b9c24eb0aca07ba4654828b4e988142
Eiafuawn <kenan.henzelin@proton.me>
2026-03-04 18:16:19 +0100

    fix: build + no CD on push
    CD fails bcs of memory

--- docs/_nuxt/wG8bQe9s.js
+++
@@ -1,1 +0,0 @@
-import{Y as r,s as i,Z as l,q as c,y as u}from"./DKX7cMcr.js";const f=r(async()=>{let e,a;const t=i("user");if(t.value)return;const s=c();try{const n=([e,a]=l(()=>$fetch("/api/auth/me",{baseURL:s.public.apiUrl,credentials:"include"})),e=await e,a(),e);if(t.value=n.user??null,!t.value)return u("/login")}catch{return t.value=null,u("/login")}});export{f as default};
