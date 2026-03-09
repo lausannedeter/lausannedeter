a9b1d0d5f9a4c3fd724e1cda766f7bf71e968f53
Eiafuawn <kenan.henzelin@proton.me>
2026-03-09 21:54:10 +0100

    chore: build

---
+++ docs/_nuxt/DLQ_vmPm.js
@@ -0,0 +1,1 @@
+import{$ as r,y as i,a0 as l,x as c,A as u}from"./D17ZZdxQ.js";const f=r(async()=>{let e,a;const t=i("user");if(t.value)return;const s=c();try{const n=([e,a]=l(()=>$fetch("/api/auth/me",{baseURL:s.public.apiUrl,credentials:"include"})),e=await e,a(),e);if(t.value=n.user??null,!t.value)return u("/login")}catch{return t.value=null,u("/login")}});export{f as default};
---
+++ docs/_nuxt/DRAC1BzC.js
@@ -0,0 +1,1 @@
+import"./D17ZZdxQ.js";const e=""+new URL("pdf.worker.CliDBb4N.mjs",import.meta.url).href;export{e as default};
