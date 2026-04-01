2004eb3257375be28048da165923598cbb25046c
David Wismer <david@thewismer.ch>
2026-04-01 16:33:30 +0200

    ??

--- pages/archives/index.vue
+++ pages/archives/index.vue
@@ -7,6 +7,8 @@ const { data: _affiches } = await useAsyncData('affiches', () =>
 );
 const affiches = _affiches.value.data.sort((a, b) => new Date(b.monthStart) - new Date(a.monthStart)) ?? [];
 
+console.log(affiches)
+
 
 const resolvedUrls = ref({})
 
