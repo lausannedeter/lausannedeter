db2ad6331aec778a6e8ca5188e1d959d8a760d0d
Kenan Henzelin <135265245+Eiafuawn@users.noreply.github.com>
2026-06-02 08:13:26 +0000

    fix: orga event endpoint

---
+++ docs/_nuxt/BVcr3ynr.js
@@ -0,0 +1,1 @@
+import{O as r,z as i,P as l,M as u,I as c}from"./kjL6tct8.js";const f=r(async()=>{let e,a;const t=i("user");if(t.value)return;const s=c();try{const n=([e,a]=l(()=>$fetch("/api/auth/me",{baseURL:s.public.apiUrl,credentials:"include"})),e=await e,a(),e);if(t.value=n.user??null,!t.value)return u("/login")}catch{return t.value=null,u("/login")}});export{f as default};
